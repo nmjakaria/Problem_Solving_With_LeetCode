3a6ee15e208734d4614d8c39f7f0ad54aefc3242
Naimullah Md Jakaria <167606035+nmjakaria@users.noreply.github.com>
2026-07-12 08:25:47 +0600

    Time: 28 ms (95.41%), Space: 73.2 MB (24.77%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,22 +3,33 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    nums.sort((a, b) => a - b);
-    const ans = [];
+    nums.sort((a, b) => a - b)
+    const res = []
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) continue; // duplicate skip
+    for (let p1 = 0; p1 < nums.length-2; p1++) {
+        if (p1 > 0 && nums[p1] === nums[p1 - 1]) continue;
+        if (nums[p1] > 0 ) break
 
-        const seen = new Set();
-        for (let j = i + 1; j < nums.length; j++) {
-            const complement = -nums[i] - nums[j];
-            if (seen.has(complement)) {
-                ans.push([nums[i], complement, nums[j]]);
-                while (j + 1 < nums.length && nums[j] === nums[j + 1]) j++;
+        let left = p1 + 1
+        let right = nums.length - 1
+
+        while (left < right) {
+            let sum = nums[p1] + nums[left] + nums[right]
+            if (sum === 0) {
+                res.push([nums[p1], nums[left], nums[right]])
+
+                while (left < right && nums[left] === nums[left+1]) left++
+                while (left < right && nums[right] === nums[right-1]) right--
+
+                left++
+                right--
+            } else if (sum > 0) {
+                right--
+            } else {
+                left++
             }
-            seen.add(nums[j]);
         }
     }
 
-    return ans;
+    return res
 };
